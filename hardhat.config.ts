0794d19c9ccbb66e5945717077eff5261a6a2b53
yuma <mr.yuma.tanaka@gmail.com>
2022-07-24 11:00:39 -0400

    initial commit

---
+++ hardhat.config.ts
@@ -0,0 +1,59 @@
+import "@typechain/hardhat"
+import "@nomiclabs/hardhat-waffle"
+import "@nomiclabs/hardhat-etherscan"
+import "@nomiclabs/hardhat-ethers"
+import "hardhat-gas-reporter"
+import "dotenv/config"
+import "solidity-coverage"
+import "hardhat-deploy"
+import "solidity-coverage"
+import { HardhatUserConfig } from "hardhat/types"
+
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL || "https://eth-rinkeby"
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0xkey"
+
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "key"
+
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "key"
+
+/** @type import('hardhat/config').HardhatUserConfig */
+const config: HardhatUserConfig = {
+    solidity: "0.8.9",
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+        },
+        localhost: {
+            chainId: 31337,
+        },
+        rinkeby: {
+            chainId: 4,
+            url: RINKEBY_RPC_URL,
+            accounts: [PRIVATE_KEY],
+        },
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+        player: {
+            default: 1,
+        },
+    },
+    gasReporter: {
+        enabled: true,
+        outputFile: "gas-report.txt",
+        noColors: true,
+        currency: "USD",
+        coinmarketcap: COINMARKETCAP_API_KEY,
+    },
+    mocha: {
+        timeout: 300 * 1000,
+    },
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
+    },
+}
+
+export default config
